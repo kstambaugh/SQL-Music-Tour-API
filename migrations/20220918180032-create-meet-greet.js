f55bd589b452e28fdf600d13fb23fb7ebf3e7e12
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-09-18 14:04:59 -0400

    fixed spelling error

--- migrations/20220918180032-create-meet-greet.js
+++ migrations/20220918180032-create-meet-greet.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('meet_mreets', {
+    await queryInterface.createTable('meet_greets', {
       meet_greet_id: {
         allowNull: false,
         autoIncrement: true,
